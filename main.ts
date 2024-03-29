f64b004d6df87d5b6c2dde9a5044d1ec14a1248d
lazy <ddlazy@qq.com>
2024-03-29 11:10:49 +0800

    chore: 更新依赖

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue';
 import App from './packages/App.vue';
 import ddlazyUi from './packages/components';
+import { createPinia } from 'pinia';
 import './packages/components/assets/main.css';
 import './packages/components/assets/var.css';
 
-createApp(App).use(ddlazyUi).mount('#app');
+createApp(App).use(createPinia()).use(ddlazyUi).mount('#app');
