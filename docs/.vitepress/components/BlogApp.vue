4a7e4e96f5e8cfcb085c02cf5ad4b4aee1933b80
lazy <ddlazy@qq.com>
2024-04-07 14:33:41 +0800

    docs: 修正文档样式

--- docs/.vitepress/components/BlogApp.vue
+++ docs/.vitepress/components/BlogApp.vue
@@ -91,57 +91,6 @@ provide('toggle-appearance', async ({ clientX: x, clientY: y }: MouseEvent) => {
 	</Layout>
 </template>
 <style lang="scss">
-.home {
-	margin: 0 auto;
-	padding: 20px;
-	max-width: 1126px;
-}
-
-@media screen and (min-width: 960px) {
-	.home {
-		padding-top: var(--vp-nav-height);
-	}
-}
-
-.header-banner {
-	width: 100%;
-	padding: 60px 0;
-}
-
-.content-wrapper {
-	display: flex;
-	align-items: flex-start;
-	justify-content: center;
-}
-
-.blog-list-wrapper {
-	width: 100%;
-}
-.blog-info-wrapper {
-	margin-left: 16px;
-	position: sticky;
-	top: 100px;
-}
-
-@media screen and (max-width: 959px) {
-	.blog-info-wrapper {
-		margin-left: 16px;
-		position: sticky;
-		top: 40px;
-	}
-}
-
-@media screen and (max-width: 767px) {
-	.content-wrapper {
-		flex-wrap: wrap;
-	}
-
-	.blog-info-wrapper {
-		margin: 20px 0;
-		width: 100%;
-	}
-}
-
 ::view-transition-old(root),
 ::view-transition-new(root) {
 	animation: none;
