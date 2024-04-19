b91c2f998bcc51eda7230c156f58ad1395d711f1
lazy <ddlazy@qq.com>
2024-04-19 16:43:16 +0800

    chore: 移除pinia

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
 import { createApp } from 'vue';
 import App from './packages/App.vue';
 import ddlazyUi from './packages/components';
-import { createPinia } from 'pinia';
 import './packages/components/assets/main.css';
 import './packages/components/assets/var.css';
 
-createApp(App).use(createPinia()).use(ddlazyUi).mount('#app');
+createApp(App).use(ddlazyUi).mount('#app');
