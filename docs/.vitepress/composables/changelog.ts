54894b9f2f8a34c9bec2dc982517948cd20c85f5
Lazy <ddlazy@qq.com>
2026-04-01 10:14:40 +0800

    docs: 修复文档报错

--- docs/.vitepress/composables/changelog.ts
+++ docs/.vitepress/composables/changelog.ts
@@ -1,6 +1,5 @@
-import { computed } from 'vue';
-import type { MaybeRefOrGetter } from '@vueuse/core';
-import { toValue } from '@vueuse/core';
+import { computed, toValue } from 'vue';
+import type { MaybeRefOrGetter } from 'vue';
 import type { CommitInfo } from '../../../scripts/types/changelog';
 import { siteName } from '../meta';
 export const useCommits = (allCommits: CommitInfo[], path: MaybeRefOrGetter<string>) => {
