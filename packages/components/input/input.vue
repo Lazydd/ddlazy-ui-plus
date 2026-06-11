603ce4d3a18f78a81b5bded085e4819eaf1a8725
Lazy <ddlazy@qq.com>
2026-06-11 12:22:47 +0800

    refrect: 调整input响应式

--- packages/components/input/input.vue
+++ packages/components/input/input.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { createName } from '../../utils/index';
-import { inputProps } from './types';
+import { inputProps, InputProps } from './types';
 
-import { ref, computed, useSlots, useTemplateRef } from 'vue';
+import { ref, computed, useTemplateRef } from 'vue';
 
 import Close from './icon/close.vue';
 import EyeOutlined from './icon/eye-outlined.vue';
@@ -12,24 +12,29 @@ defineOptions({
 	name: createName('input'),
 });
 
-const props = defineProps(inputProps);
-const emit = defineEmits(['update:value', 'change', 'clear', 'input', 'search']);
-const slots = useSlots();
+const { maxlength, allowClear, disabled, readonly, type, loading } = defineProps(inputProps);
+const modelValue = defineModel<InputProps['value']>('value')
+const emit = defineEmits<{
+	change: [value: InputProps['value']];
+	clear: [];
+	input: [value: InputProps['value']];
+	search: [value: InputProps['value']];
+}>();
 
 const InputRef = useTemplateRef('input');
 const InputTextAreaRef = useTemplateRef('inputTextArea');
 
 const handleInput = (e) => {
-	if (props.maxlength && e.target.value.length > props.maxlength) {
-		e.target.value = e.target.value.substring(0, props.maxlength);
+	if (maxlength && e.target.value.length > maxlength) {
+		e.target.value = e.target.value.substring(0, maxlength);
 		return;
 	}
-	emit('update:value', e.target.value);
+	modelValue.value = e.target.value
 	emit('change', e.target.value);
 };
 
 const clear = () => {
-	emit('update:value', '');
+	modelValue.value = ''
 	emit('change', '');
 	emit('clear');
 	emit('input', '');
@@ -37,10 +42,10 @@ const clear = () => {
 };
 
 const showClear = computed(
-	() => props.allowClear && !props.disabled && !props.readonly && props.value
+	() => allowClear && !disabled && !readonly && modelValue.value
 );
 const showPwdVisible = computed(
-	() => props.type == 'password' && !props.disabled && !props.readonly && props.value
+	() => type == 'password' && !disabled && !readonly && modelValue.value
 );
 const passwordVisible = ref(false);
 const passwordIcon = computed(() => (passwordVisible.value ? EyeOutlined : EyeInvisibleOutlined));
@@ -50,10 +55,10 @@ const handlePasswordVisible = () => {
 	InputRef.value.focus();
 };
 
-const inputCount = computed(() => props.value?.toString().length || 0);
+const inputCount = computed(() => modelValue.value?.toString().length || 0);
 
 const searchChange = () => {
-	if (props.loading) return;
+	if (loading) return;
 	emit('search', InputRef.value.value);
 };
 
@@ -65,51 +70,29 @@ defineExpose({
 </script>
 
 <template>
-	<span
-		:class="[
-			size,
-			'dd-input-base',
-			search ? 'dd-input-search' : 'dd-input-affix-wrapper',
-			{
-				'dd-input-textarea-affix-wrapper': type === 'textarea',
-				'dd-input-affix-wrapper-disabled': disabled,
-			},
-		]"
-	>
+	<span :class="[
+		size,
+		'dd-input-base',
+		search ? 'dd-input-search' : 'dd-input-affix-wrapper',
+		{
+			'dd-input-textarea-affix-wrapper': type === 'textarea',
+			'dd-input-affix-wrapper-disabled': disabled,
+		},
+	]">
 		<template v-if="type !== 'textarea'">
-			<div class="dd-input-prefix" v-if="slots.prefix">
+			<div class="dd-input-prefix" v-if="$slots?.prefix">
 				<slot name="prefix" />
 			</div>
-			<input
-				ref="input"
-				:class="['dd-input', { 'dd-input-affix-wrapper': search }]"
-				v-bind="$attrs"
-				:value
-				:readonly
-				:type="type == 'password' ? (passwordVisible ? 'text' : 'password') : type"
-				:disabled
-				@input="handleInput"
-				@keyup.enter.native="searchChange"
-			/>
-			<span
-				:class="['dd-input-group-addon', { 'dd-input-group-addon-disabled': disabled }]"
-				v-if="search"
-				type="submit"
-				@click="searchChange"
-			>
-				<svg
-					v-if="!loading"
-					focusable="false"
-					data-icon="search"
-					width="1em"
-					height="1em"
-					fill="currentColor"
-					aria-hidden="true"
-					viewBox="64 64 896 896"
-				>
+			<input ref="input" :class="['dd-input', { 'dd-input-affix-wrapper': search }]" v-bind="$attrs"
+				v-model="modelValue" :readonly
+				:type="type == 'password' ? (passwordVisible ? 'text' : 'password') : type" :disabled
+				@input="handleInput" @keyup.enter.native="searchChange" />
+			<span :class="['dd-input-group-addon', { 'dd-input-group-addon-disabled': disabled }]" v-if="search"
+				type="submit" @click="searchChange">
+				<svg v-if="!loading" focusable="false" data-icon="search" width="1em" height="1em" fill="currentColor"
+					aria-hidden="true" viewBox="64 64 896 896">
 					<path
-						d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0011.6 0l43.6-43.5a8.2 8.2 0 000-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"
-					/>
+						d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0011.6 0l43.6-43.5a8.2 8.2 0 000-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z" />
 				</svg>
 				<svg class="dd-circular" viewBox="0 0 50 50" width="1em" height="1em" v-else>
 					<circle class="path" cx="25" cy="25" r="20" fill="none" />
@@ -117,12 +100,8 @@ defineExpose({
 			</span>
 			<span class="dd-input-suffix" v-else>
 				<Close v-show="showClear" class="dd-input-clear-icon" @click="clear" />
-				<component
-					v-if="showPwdVisible"
-					class="dd-input-password-icon"
-					:is="passwordIcon"
-					@click="handlePasswordVisible"
-				/>
+				<component v-if="showPwdVisible" class="dd-input-password-icon" :is="passwordIcon"
+					@click="handlePasswordVisible" />
 				<slot name="suffix" />
 				<span class="dd-input-data-count" v-if="showCount">
 					{{ maxlength ? `${inputCount} / ${maxlength}` : inputCount }}
@@ -130,15 +109,8 @@ defineExpose({
 			</span>
 		</template>
 		<template v-else>
-			<textarea
-				ref="inputTextArea"
-				class="dd-input"
-				:value
-				:readonly
-				v-bind="$attrs"
-				:maxlength
-				@input="handleInput"
-			/>
+			<textarea ref="inputTextArea" class="dd-input" v-model="modelValue" :readonly v-bind="$attrs" :maxlength
+				@input="handleInput" />
 			<span class="dd-input-data-count" v-if="showCount">
 				{{ maxlength ? `${inputCount} / ${maxlength}` : inputCount }}
 			</span>
